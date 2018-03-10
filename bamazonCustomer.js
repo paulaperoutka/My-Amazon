a42f0410fe90b6c469cb4e808b68ec7b8258ef1d
paulaperoutka <paulaperoutka@gmail.com>
2018-03-10 09:50:11 -0500

    notes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+// Required node packages
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+//local connection
 var connection = mysql.createConnection({
   host: "localhost",
   port: "3306",
@@ -9,6 +11,7 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
+//connect
 connection.connect((error) => {
   if (error) {
     throw error;
@@ -17,19 +20,21 @@ connection.connect((error) => {
   stockStore();
 });
 
+//function for listing store products
 function stockStore () {
   console.log("\n\nWELCOME TO BAMAZON!\n----------------------\nA site for unique & unrelated items you didn't know you needed.\n----------------------\n\nSHOP the following products:\n");
-
+//make query to mysql db
   connection.query("SELECT * FROM products", (error, response) => {
       if (error) {
         throw error;
         console.log ("Hmm...looks like we have some stocking issues. Please come back later!")
       }
-
+//grab each object from the connection query response object
       response.forEach(products => {
         console.log(products.id + " - " + products.product_name + "\nPrice: $" + products.price + "\n" + products.department_name + " | ID#: " 
               + products.item_id + "\n");
-      });   
+      }); 
+//call inquirer-based function to start "buying" process
     buyItem();  
   });
 };
@@ -38,6 +43,7 @@ function buyItem() {
 
   console.log("----------------------\nCART & CHECKOUT\n----------------------\n\nNeed something you didn't know you needed?  We won't say we told you so!  \nStart the checkout process below...\n(Type quit at any time to leave us)\n");
 
+//request item to buy from user
   inquirer.prompt([
     {
       type: "input",
@@ -46,10 +52,11 @@ function buyItem() {
     }
   ]).then((response) => {
 
+//option to quit the app
     if (response.id === "quit") {
       connection.end();
     }
-
+//else take response and make db query for that item
     else {
       let productId = parseInt(response.id);
       
@@ -63,7 +70,9 @@ function buyItem() {
         if (response && productId === response[0].item_id) {
           console.log("You have selected ID# " + productId + ": " + response[0].product_name + ".");
           itemQuantity(response[0].item_id);
-        } 
+        }
+
+//handle id # that doesnt exist 
         else {
           console.log ("\nWe don't seem to have a product matching that ID#.  Shop again. \n\n");
           buyItem();
@@ -73,6 +82,7 @@ function buyItem() {
   });
 };
 
+//function using inquirer to get amount of item to buy
 function itemQuantity (id) {
 
   let ID = parseInt(id);
@@ -89,6 +99,7 @@ function itemQuantity (id) {
       connection.end();
     }
 
+//take quantity from user and subtract from db if sufficient quantity
     else {
       let quantity = parseInt(response.quantity);
       console.log("You wish to purchase " + quantity + ".");
@@ -100,7 +111,7 @@ function itemQuantity (id) {
         if (error) {
           throw err; 
         }
-
+//if sufficient quantity,update db
         if (quantity <= response[0].stock_quantity) {
           connection.query("UPDATE products SET ? WHERE ?", [
             {
@@ -119,6 +130,7 @@ function itemQuantity (id) {
           });
         } 
         
+//if insufficient quantity, don't update db
         else {
           console.log("Oh no!  We don't have enough in stock to complete your order.  Please try a smaller quantity or a different item.")
           stockStore();
